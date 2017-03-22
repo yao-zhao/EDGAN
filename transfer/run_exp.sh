dea5f81c84fe031e3dcad1bad3048eae09ecce7e
Yao Zhao <jimzhaoshine@gmail.com>
2017-03-22 15:03:53 -0700

    update param

--- transfer/run_exp.sh
+++ transfer/run_exp.sh
@@ -1,3 +1,3 @@
-python2 transfer/train.py --max_epoch 6 --batch_size 16 --num_epochs_per_decay 2 \
---is_training True --initial_learning_rate 1e-3 --stddev 5e-2 --weight_decay 1e-6 \
+python2 transfer/train.py --max_epoch 3 --batch_size 16 --num_epochs_per_decay 1 \
+--is_training True --initial_learning_rate 1e-2 --stddev 5e-2 --weight_decay 1e-6 \
 --minimal_summaries False
