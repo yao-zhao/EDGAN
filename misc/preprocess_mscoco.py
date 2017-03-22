4cd0fc56152b1b508ca527054ed693293a07aedf
Yao Zhao <jimzhaoshine@gmail.com>
2017-03-21 22:26:21 -0700

    use major filtering

--- misc/preprocess_mscoco.py
+++ misc/preprocess_mscoco.py
@@ -23,42 +23,9 @@ DEBUG = False
 # DEBUG = True
 FILTER_ASPECT_RATIO = True
 ASPECT_RATIO = 9/16
-
-def save_data_seperate(inpath, outpath):
-    raise NotImplementedError    
-    filenames = os.listdir(inpath)
-    filenames.sort()
-    print('number of training images: '+str(len(filenames)))
-    lr_size = int(LOAD_SIZE / LR_HR_RETIO)
-    lr_path = os.path.join(outpath, 'train_lr')
-    hr_path = os.path.join(outpath, 'train_hr')
-    for filename in filenames:
-        img = cv2.imread(os.path.join(inpath, filename))
-        if KEEP_RATIO:
-            h = float(img.shape[0])
-            w = float(img.shape[1])
-            minshape = np.min([h, w])
-            img = cv2.resize(img,
-                (int(IMSIZE*w/minshape), int(IMSIZE*h/minshape)),
-                interpolation=CV_FLAG)
-            cv2.imwrite(os.path.join(hr_path, filename), img)
-            print(filename, int(IMSIZE*h/minshape), int(IMSIZE*w/minshape))
-            img = cv2.resize(img,
-                (int(lr_size*w/minshape), int(lr_size*h/minshape)),
-                interpolation=CV_FLAG)
-            cv2.imwrite(os.path.join(lr_path, filename), img)
-            print(filename, int(lr_size*h/minshape), int(lr_size*w/minshape))
-        else:
-            img = cv2.resize(img, (IMSIZE, IMSIZE),
-                interpolation=CV_FLAG)
-            cv2.imwrite(os.path.join(hr_path, filename), img)
-            img = cv2.resize(img, (lr_size, lr_size),
-                interpolation=CV_FLAG)
-            cv2.imwrite(os.path.join(lr_path, filename), img)
-            print(filename)
+AREA_TH = 0.01
             
-def get_ImageIds(annFile, selected_supers):    
-    coco=COCO(annFile)
+def get_ImageIds(coco, selected_supers):
     cats = coco.loadCats(coco.getCatIds())
     selected_subs = []
     for cat in cats:
@@ -70,7 +37,6 @@ def get_ImageIds(annFile, selected_supers):
         catIds = coco.getCatIds(catNms=sub);
         imgIds.extend(coco.getImgIds(catIds=catIds))
     imgIds = list(np.unique(imgIds))
-    all_count = len(imgIds)
     image_names = []
     for img in coco.loadImgs(imgIds):
         if FILTER_ASPECT_RATIO is True:
@@ -83,25 +49,50 @@ def get_ImageIds(annFile, selected_supers):
         % (len(image_names) / float(len(imgIds))))
     return image_names
 
-def save_embedding(inpath, outpath):
-    raise NotImplementedError    
-    filenames = os.listdir(inpath)
-    filenames.sort()
-    print('number of training images: '+str(len(filenames)))
-    numfiles = len(filenames)
-    for i, filename in zip(range(numfiles), filenames):
-        if i % int(numfiles/100) == 0:
-            t_file = torchfile.load(os.path.join(inpath, filename))
-            print('process %.2f'% (i * 1. / numfiles))
+def get_ImageIds_Major(coco, selected_supers, exceptions=[]):
+    cats = coco.loadCats(coco.getCatIds())
+    selected_subs = []
+    for cat in cats:
+        if cat['supercategory'] in selected_supers:
+            selected_subs.append((cat['name']))
+    print('chosen sub classes: '+' '.join(selected_subs))
+    imgIds = []
+    for sub in selected_subs:
+        catIds = coco.getCatIds(catNms=sub);
+        imgIds.extend(coco.getImgIds(catIds=catIds))
+    imgIds = list(np.unique(imgIds))
+    allowed_subs = selected_subs + exceptions
+    allowed_ids = coco.getCatIds(catNms=allowed_subs);
+    image_names = []
+    for img in coco.loadImgs(imgIds):
+        if FILTER_ASPECT_RATIO is True:
+            aspect_ratio = img['height']/img['width']
+            if aspect_ratio < ASPECT_RATIO or aspect_ratio > 1/ASPECT_RATIO:
+                continue
 
+        annIds = coco.getAnnIds(imgIds=img['id'], iscrowd=None)
+        annotations = coco.loadAnns(annIds)
+        pass_filter = True
+        img_area = img['height'] * img['width']
+        areas = [anno['area'] for anno in annotations]
+        areas.sort()
+        third_max = 0 if len(areas) < 3 else areas[2]
+        for anno in annotations:
+            if anno['category_id'] not in allowed_ids and \
+                anno['area'] > third_max and\
+                anno['area'] > AREA_TH*img_area:
+                pass_filter = False
+                break
+        if pass_filter is False:
+            continue
+
+        image_names.append(img['file_name'])
+    print('%0.2f percent images (%d) left after filtering' \
+        % (len(image_names) / float(len(imgIds)), len(image_names)))
+    return image_names
 
 def save_tfrecords(imagepath, embeddingpath, outpath,
-                   annoFile=None, selected_supers=None, tag=''):
-    if annoFile is None or selected_supers is None:
-        filenames = os.listdir(imagepath)
-        filenames.sort()
-    else:
-        filenames = get_ImageIds(annoFile, selected_supers)
+                   filenames, tag=''):
     if DEBUG: filenames = filenames[:100]
     print('number of training images: '+str(len(filenames)))
     lr_size = int(LOAD_SIZE / LR_HR_RETIO)
@@ -220,12 +211,64 @@ def test_tfrecords(tfrecords_filename):
 if __name__ == '__main__':
     train_dir = os.path.join(COCO_DIR, 'train2014/')
     embed_dir = os.path.join(COCO_DIR, 'train2014_ex_t7')
-    selected_supers = \
-        ['furniture', 'appliance']
-    save_tfrecords(train_dir, embed_dir, COCO_DIR, tag='_fur_app',
-        annoFile=os.path.join(COCO_DIR, 'annotations', 'instances_train2014.json'),
-        selected_supers=selected_supers)
+    annoFile=os.path.join(COCO_DIR, 'annotations', 'instances_train2014.json')
+    coco=COCO(annoFile)
+    if False:
+        selected_supers = \
+            ['furniture', 'appliance']
+        filenames = get_ImageIds(coco, selected_supers)
+        save_tfrecords(train_dir, embed_dir, COCO_DIR, filenames, tag='_fur_app')
+
+    if True:
+        selected_supers = \
+            ['furniture', 'appliance']
+        filenames = get_ImageIds_Major(coco, selected_supers, exceptions=['people'])
+        save_tfrecords(train_dir, embed_dir, COCO_DIR, filenames, tag='_fur_app_major')
+
     # test_tfrecords(os.path.join(COCO_DIR, '76.tfrecords'))
     # test_tfrecords(os.path.join(COCO_DIR, '304.tfrecords'))
 
 
+def save_data_seperate(inpath, outpath):
+    raise NotImplementedError
+    filenames = os.listdir(inpath)
+    filenames.sort()
+    print('number of training images: '+str(len(filenames)))
+    lr_size = int(LOAD_SIZE / LR_HR_RETIO)
+    lr_path = os.path.join(outpath, 'train_lr')
+    hr_path = os.path.join(outpath, 'train_hr')
+    for filename in filenames:
+        img = cv2.imread(os.path.join(inpath, filename))
+        if KEEP_RATIO:
+            h = float(img.shape[0])
+            w = float(img.shape[1])
+            minshape = np.min([h, w])
+            img = cv2.resize(img,
+                (int(IMSIZE*w/minshape), int(IMSIZE*h/minshape)),
+                interpolation=CV_FLAG)
+            cv2.imwrite(os.path.join(hr_path, filename), img)
+            print(filename, int(IMSIZE*h/minshape), int(IMSIZE*w/minshape))
+            img = cv2.resize(img,
+                (int(lr_size*w/minshape), int(lr_size*h/minshape)),
+                interpolation=CV_FLAG)
+            cv2.imwrite(os.path.join(lr_path, filename), img)
+            print(filename, int(lr_size*h/minshape), int(lr_size*w/minshape))
+        else:
+            img = cv2.resize(img, (IMSIZE, IMSIZE),
+                interpolation=CV_FLAG)
+            cv2.imwrite(os.path.join(hr_path, filename), img)
+            img = cv2.resize(img, (lr_size, lr_size),
+                interpolation=CV_FLAG)
+            cv2.imwrite(os.path.join(lr_path, filename), img)
+            print(filename)
+
+def save_embedding(inpath, outpath):
+    raise NotImplementedError
+    filenames = os.listdir(inpath)
+    filenames.sort()
+    print('number of training images: '+str(len(filenames)))
+    numfiles = len(filenames)
+    for i, filename in zip(range(numfiles), filenames):
+        if i % int(numfiles/100) == 0:
+            t_file = torchfile.load(os.path.join(inpath, filename))
+            print('process %.2f'% (i * 1. / numfiles))
